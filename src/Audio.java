897ceea38d99fbc1e725eddd19f9840d422b10d3
Lucas Neves <lucas.ccdr@gmail.com>
2024-10-10 19:54:36 -0300

    Creating the getters and setters to access and modify the attributes.

--- src/Audio.java
+++ src/Audio.java
@@ -1,7 +1,38 @@
 public class Audio {
-    String title;
-    int totalReproductions;
-    int totalLikes;
-    int classification;
+    private String title;
+    private int totalReproductions;
+    private int totalLikes;
+    private int classification;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTotalReproductions() {
+        return totalReproductions;
+    }
+
+    public void setTotalReproductions(int totalReproductions) {
+        this.totalReproductions = totalReproductions;
+    }
+
+    public int getTotalLikes() {
+        return totalLikes;
+    }
+
+    public void setTotalLikes(int totalLikes) {
+        this.totalLikes = totalLikes;
+    }
+
+    public int getClassification() {
+        return classification;
+    }
+
+    public void setClassification(int classification) {
+        this.classification = classification;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Audio audio = new Audio();
+
+        audio.setTitle("Firestorm");
+        audio.setTotalReproductions(589);
+        audio.setClassification(5);
+        audio.setTotalLikes(1578);
+
+        System.out.println("Nome da música: " + audio.getTitle());
+        System.out.println("Foi reproduzida " + audio.getTotalReproductions() + " vezes");
+        System.out.println("Possui uma classificação de " + audio.getClassification() + " estrelas");
+        System.out.println(audio.getTotalLikes() + " pessoas gostaram da música.");
+
+
+
     }
 }
