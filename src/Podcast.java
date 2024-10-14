60b36f8b56428d4507bb1e579f9343784c3d8972
Lucas Neves <lucas.ccdr@gmail.com>
2024-10-14 16:16:27 -0300

    Created two classes Music and Podcast

---
+++ src/Podcast.java
@@ -0,0 +1,20 @@
+public class Podcast extends Audio{
+    private String presenter;
+    private String description;
+
+    public String getPresenter() {
+        return presenter;
+    }
+
+    public void setPresenter(String presenter) {
+        this.presenter = presenter;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
